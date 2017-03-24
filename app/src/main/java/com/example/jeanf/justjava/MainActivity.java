b9f353408df88d1dc4cc17be2f2b75946f18b248
jsoft54 <jsoft54@aoutlook.com>
2017-03-23 21:19:59 -0500

    Hatsa Video9 object oriented 2

--- app/src/main/java/com/example/jeanf/justjava/MainActivity.java
+++ app/src/main/java/com/example/jeanf/justjava/MainActivity.java
@@ -4,6 +4,8 @@ import java.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,8 +25,17 @@ public class MainActivity extends AppCompatActivity {
     }
     public String createOrderSummary(int totalPrice)
     {
-        return "Name: JF" + "\n" + "Quantity:" + quantity + "\n" + "Total: "
-            + NumberFormat.getCurrencyInstance().format(totalPrice) +"\nTank You!!" ;
+        boolean creamChecked = false;
+        creamChecked = ((CheckBox) findViewById(R.id.creamCheck)).isChecked();
+        boolean chocolateChecked = false;
+        chocolateChecked = ((CheckBox) findViewById(R.id.chocolateCheck)).isChecked();
+        String nameOrder = ((EditText) findViewById(R.id.nameText)).getText().toString();
+        return "Name:" + nameOrder + "\n"
+                +  "Add whipped cream? " + creamChecked + "\n"
+                +  "Add chocolate? " + chocolateChecked + "\n"
+                + "Quantity:" + quantity + "\n"
+                + "Total: "      + NumberFormat.getCurrencyInstance().format(totalPrice)
+                +"\nTank You!!" ;
     }
     public void displayMessage(String message){
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
